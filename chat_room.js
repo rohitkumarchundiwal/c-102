523965ba9e67c0bb69ed46d38c777ae9fe86198e
rohitkumarchundiwal <124276493+rohitkumarchundiwal@users.noreply.github.com>
2023-06-03 08:53:32 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgEiTWeFrTfteOncVbTaM5WYYu0nSYEw0",
+    authDomain: "lets-chat-web-app-d52d8.firebaseapp.com",
+    projectId: "lets-chat-web-app-d52d8",
+    storageBucket: "lets-chat-web-app-d52d8.appspot.com",
+    messagingSenderId: "901881272266",
+    appId: "1:901881272266:web:564b4f4ed3c5512932a133",
+}
